fc0db402b6ec5c732034c1df90906cca2d2ea68b
InvalidNickname <alesha.kiselyov@gmail.com>
2020-12-17 20:26:15 +0300

    - https check

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,37 +1,26 @@
 import axios from 'axios';
-import * as https from "https";
-let fs = require('fs');
 
 const API_URL = process.env.API_URL || 'https://ec2-3-138-245-221.us-east-2.compute.amazonaws.com:3001/';
-
-const instance = axios.create({
-    baseURL: API_URL,
-    httpsAgent: new https.Agent({
-        cert: fs.readFileSync(`../../nginx-selfsigned.crt`),
-        key: fs.readFileSync(`../../nginx-selfsigned.key`),
-        rejectUnauthorized: false
-    })
-})
 
 class UserService {
 
     async getGameState(id) {
-        const {data: state} = await instance.get(API_URL + "state/" + id)
+        const {data: state} = await axios.get(API_URL + "state/" + id)
         return state
     }
 
     async makeMove(id, i, j, toI, toJ) {
-        const {data: state} = await instance.post(API_URL + "move/" + id, {i, j, toI, toJ})
+        const {data: state} = await axios.post(API_URL + "move/" + id, {i, j, toI, toJ})
         return state
     }
 
     async createGame() {
-        const {data: state} = await instance.get(API_URL + "create")
+        const {data: state} = await axios.get(API_URL + "create")
         return state.id
     }
 
     async getHighlight(id, i, j) {
-        const {data: highlight} = await instance.post(API_URL + "highlight/" + id, {i, j})
+        const {data: highlight} = await axios.post(API_URL + "highlight/" + id, {i, j})
         return highlight
     }
 }
